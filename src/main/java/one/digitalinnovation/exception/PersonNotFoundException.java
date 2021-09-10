8bafb461997e029a5fd9501f914e115601221fe6
thayklain <thay.klain@gmail.com>
2021-09-10 03:02:02 -0300

    tried to make it work and failed

---
+++ src/main/java/one/digitalinnovation/exception/PersonNotFoundException.java
@@ -0,0 +1,12 @@
+package one.digitalinnovation.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class PersonNotFoundException extends Exception {
+
+    public PersonNotFoundException(long id) {
+        super("Person not found wit ID " + id);
+    }
+}
